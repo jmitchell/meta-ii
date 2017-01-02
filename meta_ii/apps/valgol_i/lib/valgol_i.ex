f1cd4517023e986f2914d97a750de6cd2a71855a
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-02 11:32:40 -0800

    add VALGOL I assembly parse test
    Currently fails because implementation still assumes fixed 4 bytes per
    instruction. Based on the address annotations in Figure 3 of the
    paper, this assumption is clearly wrong.

--- meta_ii/apps/valgol_i/lib/valgol_i.ex
+++ meta_ii/apps/valgol_i/lib/valgol_i.ex
@@ -1,24 +1,29 @@
 defmodule ValgolI do
   @bytes_per_word 4
 
-  def parse_assembly(lines) when is_list(lines) do
-    parse_assembly(lines, %{instructions: [], address: 0, labels: %{}})
+  def parse(src) when is_binary(src) do
+    src
+    |> String.split("\n")
+    |> parse(%{instructions: [], address: 0, labels: %{}})
   end
-  defp parse_assembly([h | t], context) do
-    case parse_assembly_line(h) do
-      {:op, code} ->
-	parse_assembly(t, %{context |
-			    instructions: [code | context.instructions],
-			    address: next_address(context.address)})
-      {:label, label} ->
-	parse_assembly(t, %{context |
-			    labels: Map.put_new(context.labels, label, context.address)})
-      {:error, reason} -> {:error, reason}
+  defp parse([h | t], context) do
+    if String.trim(h) == "" do
+      parse(t, context)
+    else
+      case h |> String.trim_trailing |> parse_line do
+	{:op, code} ->
+	  parse(t, %{context |
+		     instructions: [code | context.instructions],
+		     address: next_address(context.address)})
+	{:label, label} ->
+	  parse(t, %{context |
+		     labels: Map.put_new(context.labels, label, context.address)})
+	{:error, reason} -> {:error, reason}
+      end
     end
   end
-  defp parse_assembly([], context) do
+  defp parse([], context) do
     %{context | instructions: Enum.reverse(context.instructions)}
-    |> dereference_labels
   end
 
   defp dereference_labels(assembly) do
@@ -28,37 +33,37 @@ defmodule ValgolI do
 
   defp next_address(addr), do: addr + @bytes_per_word
 
-  def parse_assembly_line(" " <> op) do
+  def parse_line(" " <> op) do
     case parse_op(op) do
       {:error, reason} -> {:error, reason}
       :error -> {:error, "Encountered some kind of error"}
       op_code -> {:op, op_code}
     end
   end
-  def parse_assembly_line(label) do
+  def parse_line(label) do
     {:label, label}
   end
 
   defp parse_op(" " <> op), do: parse_op(op)
-  defp parse_op("LD " <> loc), do: {:load, {:location, loc}}
+  defp parse_op("LD  " <> loc), do: {:load, {:location, loc}}
   defp parse_op("LDL " <> lit) do
     case Float.parse(lit) do
       {num, _} -> {:load_literal, num}
       _ -> {:error, "LDL opcode expects a literal number argument, not '#{lit}'."}
     end
   end
-  defp parse_op("ST " <> loc), do: {:store, {:location, loc}}
+  defp parse_op("ST  " <> loc), do: {:store, {:location, loc}}
   defp parse_op("ADD"), do: :add
   defp parse_op("SUB"), do: :subtract
   defp parse_op("MLT"), do: :multiply
   defp parse_op("EQU"), do: :equal
-  defp parse_op("B " <> loc), do: {:branch, {:location, loc}}
+  defp parse_op("B   " <> loc), do: {:branch, {:location, loc}}
   defp parse_op("BFP " <> loc), do: {:branch_false, {:location, loc}}
   defp parse_op("BTP " <> loc), do: {:branch_true, {:location, loc}}
   defp parse_op("EDT " <> str), do: {:edit, str}
   defp parse_op("PNT"), do: :print
   defp parse_op("HLT"), do: :halt
-  defp parse_op("SP " <> n) do
+  defp parse_op("SP  " <> n) do
     case Integer.parse(n, 10) do
       {n, _} -> {:space, n}
       _ -> {:error, "SP opcode expects an integer argument, not '#{n}'."}
--- meta_ii/apps/valgol_i/test/valgol_i_test.exs
+++ meta_ii/apps/valgol_i/test/valgol_i_test.exs
@@ -2,7 +2,72 @@ defmodule ValgolITest do
   use ExUnit.Case
   doctest ValgolI
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @example_program """
+         B   A01
+  X
+         BLK 001
+  A01
+         LDL 0
+         ST  X
+  A02
+         LD  X
+         LDL 3
+         EQU
+         BTP A03
+         LD  X
+         LD  X
+         MLT
+         LDL 10
+         MLT
+         LDL 1
+         ADD
+         EDT 01'*'
+         PNT
+         LD  X
+         LDL 0.1
+         ADD
+         ST  X
+         B   A02
+  A03
+         HLT
+         SP  1
+         END
+  """
+
+  test "parse example program" do
+    asm = @example_program |> ValgolI.parse
+    assert asm.instructions == [
+      {:branch, {:location, "A01"}},
+      {:block, 1},
+      {:load_literal, 0.0},
+      {:store, {:location, "X"}},
+      {:load, {:location, "X"}},
+      {:load_literal, 3.0},
+      :equal,
+      {:branch_true, {:location, "A03"}},
+      {:load, {:location, "X"}},
+      {:load, {:location, "X"}},
+      :multiply,
+      {:load_literal, 10.0},
+      :multiply,
+      {:load_literal, 1.0},
+      :add,
+      {:edit, "01'*'"},
+      :print,
+      {:load, {:location, "X"}},
+      {:load_literal, 0.1},
+      :add,
+      {:store, {:location, "X"}},
+      {:branch, {:location, "A02"}},
+      :halt,
+      {:space, 1},
+      :end,
+    ]
+    assert asm.labels == %{
+      "X" => 4,
+      "A01" => 12,
+      "A02" => 25,
+      "A03" => 97,
+    }
   end
 end
