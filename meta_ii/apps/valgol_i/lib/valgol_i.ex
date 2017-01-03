a27717bdbeffa277702531611df39c3b8f319e00
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-02 17:57:55 -0800

    VALGOL I: initial assembly interpreter
    There must be a bug because the output doesn't match Figure 4 from the
    paper.

--- meta_ii/apps/valgol_i/lib/valgol_i.ex
+++ meta_ii/apps/valgol_i/lib/valgol_i.ex
@@ -1,5 +1,6 @@
 defmodule ValgolI do
   @bytes_per_word 4
+  @print_area_size 80
 
   def parse(src) when is_binary(src) do
     src
@@ -15,7 +16,7 @@ defmodule ValgolI do
 	{:op, code} ->
 	  parse(t, %{context |
 		     instructions: Map.put_new(context.instructions, context.address, code),
-		     address: next_address(context.address, code)})
+		     address: next_instruction_addr(context.address, code)})
 	{:label, label} ->
 	  # TODO: ensure label isn't already used
 	  parse(t, %{context |
@@ -26,12 +27,106 @@ defmodule ValgolI do
   end
   defp parse([], context), do: context
 
+  def interpret(memory) when is_map(memory) do
+    step(%{memory: memory})
+  end
+
+  defp step(state \\ %{}) do
+    state =
+      state
+      |> Map.put_new(:pc, 0)
+      |> Map.put_new(:stack, [])
+      |> Map.put_new_lazy(:print_area, &clean_print_area/0)
+      |> Map.put_new(:output, [])
+
+    op = Map.get(state[:memory], state[:pc])
+
+    update = fn state, kw, update_fn ->
+      {_, new_state} = Map.get_and_update(state, kw, fn val -> {val, update_fn.(val)} end)
+      new_state
+    end
+    advance_pc = state |> update.(:pc, fn pc ->
+      next_instruction_addr(pc, op)
+    end)
+    pop_stack = &(&1 |> update.(:stack, fn s -> tl s end))
+
+    case op do
+      nil ->
+	advance_pc |> step
+      {:branch, addr} ->
+	advance_pc |> step
+      {:block, n} ->
+	advance_pc |> step
+      {:load_literal, n} ->
+      	advance_pc |> update.(:stack, &[n | &1]) |> step
+      {:store, addr} ->
+	advance_pc
+	|> pop_stack.()
+	|> update.(:memory, &(Map.put(&1, addr, hd state[:stack])))
+	|> step
+      {:load, addr} ->
+	case Map.get(state[:memory], addr) do
+	  nil -> {:error, "Nothing allocated to address #{addr}"}
+	  n -> advance_pc |> update.(:stack, &[n | &1]) |> step
+	end
+      :equal ->
+	advance_pc
+	|> update.(:stack, fn [a,b | c] ->
+	  [if a == b do 1 else 0 end | c]
+	end)
+	|> step
+      {:branch_true, addr} ->
+	advance_pc
+	|> update.(:pc, &(if hd(state[:stack]) != 0 do addr else &1 end))
+	|> pop_stack.()
+	|> step
+      :add ->
+	advance_pc
+	|> update.(:stack, fn [a,b | c] -> [a+b | c] end)
+	|> step
+      :subtract ->
+	advance_pc
+	|> update.(:stack, fn [a,b | c] -> [b-a | c] end)
+	|> step
+      :multiply ->
+	advance_pc
+	|> update.(:stack, fn [a,b | c] -> [a*b | c] end)
+	|> step
+      {:edit, s} ->
+	advance_pc
+	|> update.(:print_area, fn pa ->
+	  # TODO: handle buffer overflow case.
+	  n = state[:stack] |> hd |> round
+	  String.slice(pa, 0, n) <> s <> String.slice(pa, n + String.length(s), String.length(pa) - (n + String.length(s)))
+	end)
+	|> step
+      :print ->
+	advance_pc
+	|> update.(:output, &[&1 | [state[:print_area] <> "\n"]])
+	|> update.(:print_area, fn _ -> clean_print_area end)
+	|> step
+      :halt ->
+	advance_pc |> step
+      {:space, n} ->
+	state
+	|> update.(:pc, fn pc -> pc + n end)
+	|> step
+      :end ->
+	state
+        |> update.(:output, &(&1 |> List.flatten |> Enum.join))
+      _ ->
+    	{:error, "Unsupported instruction: #{inspect op}"}
+    end
+  end
+
+  defp clean_print_area, do: String.duplicate(" ", @print_area_size)
+
   defp dereference_labels(assembly) do
     deref = fn
       {addr, {op, {:label_ref, ref}}} ->
 	case Map.get(assembly.labels, ref) do
 	  nil -> {:error, "Unrecognized label reference: '#{ref}'"}
-	  label_addr -> {addr, {op, {:address, label_addr}}}
+	  label_addr -> {addr, {op, label_addr}}
 	end
       other -> other
     end
@@ -39,18 +134,18 @@ defmodule ValgolI do
     assembly.instructions |> Enum.map(deref) |> Enum.into(%{})
   end
 
-  defp next_address(addr, :equal), do: addr + 1
-  defp next_address(addr, :multiply), do: addr + 1
-  defp next_address(addr, :add), do: addr + 1
-  defp next_address(addr, :subtract), do: addr + 1
-  defp next_address(addr, :print), do: addr + 1
-  defp next_address(addr, :halt), do: addr + 1
-  defp next_address(addr, :end), do: addr + 1
-  defp next_address(addr, {:block, n}), do: addr + (n * 8)
-  defp next_address(addr, {:load_literal, _}), do: addr + 9
-  defp next_address(addr, {:edit, _}), do: addr + 2
-  defp next_address(addr, {:space, n}), do: addr + n
-  defp next_address(addr, _code), do: addr + @bytes_per_word
+  defp next_instruction_addr(addr, :equal), do: addr + 1
+  defp next_instruction_addr(addr, :multiply), do: addr + 1
+  defp next_instruction_addr(addr, :add), do: addr + 1
+  defp next_instruction_addr(addr, :subtract), do: addr + 1
+  defp next_instruction_addr(addr, :print), do: addr + 1
+  defp next_instruction_addr(addr, :halt), do: addr + 1
+  defp next_instruction_addr(addr, :end), do: addr + 1
+  defp next_instruction_addr(addr, {:block, n}), do: addr + (n * 8)
+  defp next_instruction_addr(addr, {:load_literal, _}), do: addr + 9
+  defp next_instruction_addr(addr, {:edit, _}), do: addr + 2
+  defp next_instruction_addr(addr, {:space, n}), do: addr + n
+  defp next_instruction_addr(addr, _code), do: addr + @bytes_per_word
 
   defp parse_line(" " <> op) do
     case parse_op(op) do
@@ -79,7 +174,7 @@ defmodule ValgolI do
   defp parse_op("B   " <> loc), do: parse_location(:branch, loc)
   defp parse_op("BFP " <> loc), do: parse_location(:branch_false, loc)
   defp parse_op("BTP " <> loc), do: parse_location(:branch_true, loc)
-  defp parse_op("EDT " <> str), do: {:edit, str}
+  defp parse_op("EDT " <> str), do: parse_string(:edit, str)
   defp parse_op("PNT"), do: :print
   defp parse_op("HLT"), do: :halt
   defp parse_op("SP  " <> n) do
@@ -101,6 +196,13 @@ defmodule ValgolI do
     case Integer.parse(loc, 10) do
       {addr, _} -> {op, {:address, addr}}
       _ -> {op, {:label_ref, loc}}
+    end
+  end
+
+  defp parse_string(op, <<nn :: bytes-size(2)>> <> "'" <> str) do
+    case Integer.parse(nn) do
+      {len, _} -> {op, String.slice(str, 0, len)}
+      _ -> {:error, "Invalid string length signifier: '#{nn}'"}
     end
   end
 end
--- meta_ii/apps/valgol_i/test/valgol_i_test.exs
+++ meta_ii/apps/valgol_i/test/valgol_i_test.exs
@@ -35,35 +35,69 @@ defmodule ValgolITest do
   """
 
   @parsed_program %{
-    0 => {:branch, {:address, 12}},
+    0 => {:branch, 12},
     4 => {:block, 1},
     12 => {:load_literal, 0.0},
-    21 => {:store, {:address, 4}},
-    25 => {:load, {:address, 4}},
+    21 => {:store, 4},
+    25 => {:load, 4},
     29 => {:load_literal, 3.0},
     38 => :equal,
-    39 => {:branch_true, {:address, 97}},
-    43 => {:load, {:address, 4}},
-    47 => {:load, {:address, 4}},
+    39 => {:branch_true, 97},
+    43 => {:load, 4},
+    47 => {:load, 4},
     51 => :multiply,
     52 => {:load_literal, 10.0},
     61 => :multiply,
     62 => {:load_literal, 1.0},
     71 => :add,
-    72 => {:edit, "01'*'"},
+    72 => {:edit, "*"},
     74 => :print,
-    75 => {:load, {:address, 4}},
+    75 => {:load, 4},
     79 => {:load_literal, 0.1},
     88 => :add,
-    89 => {:store, {:address, 4}},
-    93 => {:branch, {:address, 25}},
+    89 => {:store, 4},
+    93 => {:branch, 25},
     97 => :halt,
     98 => {:space, 1},
     99 => :end,
   }
 
+  @program_output """
+  *
+  *
+  *
+   *
+    *
+     *
+      *
+       *
+         *
+           *
+             *
+               *
+                  *
+                     *
+                        *
+                           *
+                              *
+                                 *
+                                     *
+                                         *
+                                             *
+                                                 *
+                                                      *
+                                                           *
+                                                                *
+                                                                     *
+  """
+
   test "parse example program" do
     asm = @example_program |> ValgolI.parse
     assert asm == @parsed_program
+  end
+
+  test "interpret example program" do
+    state = @parsed_program |> ValgolI.interpret
+    assert state[:output] == @program_output
   end
 end
