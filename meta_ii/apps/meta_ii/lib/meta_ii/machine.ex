7c12c455d21f7018e42a483aa98f7c3d2e56699c
Jacob Mitchell <jmitchell@member.fsf.org>
2017-01-03 11:02:50 -0800

    META II machine: comment out unimplemented interpreter steps

--- meta_ii/apps/meta_ii/lib/meta_ii/machine.ex
+++ meta_ii/apps/meta_ii/lib/meta_ii/machine.ex
@@ -21,60 +21,60 @@ defmodule MetaII.Machine do
       |> update(:switch, false)
     end
   end
-  def step(state, :identifier) do
+  # def step(state, :identifier) do
 
-  end
-  def step(state, :number) do
+  # end
+  # def step(state, :number) do
 
-  end
-  def step(state, :string) do
+  # end
+  # def step(state, :string) do
 
-  end
-  def step(state, {:call, address}) do
+  # end
+  # def step(state, {:call, address}) do
 
-  end
-  def step(state, :return) do
+  # end
+  # def step(state, :return) do
 
-  end
-  def step(state, :set) do
+  # end
+  # def step(state, :set) do
 
-  end
-  def step(state, {:branch, address}) do
+  # end
+  # def step(state, {:branch, address}) do
 
-  end
-  def step(state, {:branch_true, address}) do
+  # end
+  # def step(state, {:branch_true, address}) do
 
-  end
-  def step(state, {:branch_false, address}) do
+  # end
+  # def step(state, {:branch_false, address}) do
 
-  end
-  def step(state, :branch_error) do
+  # end
+  # def step(state, :branch_error) do
 
-  end
-  def step(state, {:copy_literal, str}) do
+  # end
+  # def step(state, {:copy_literal, str}) do
 
-  end
-  def step(state, :copy_input) do
+  # end
+  # def step(state, :copy_input) do
 
-  end
-  def step(state, :generate1) do
+  # end
+  # def step(state, :generate1) do
 
-  end
-  def step(state, :generate2) do
+  # end
+  # def step(state, :generate2) do
 
-  end
-  def step(state, :label) do
+  # end
+  # def step(state, :label) do
 
-  end
-  def step(state, :output) do
+  # end
+  # def step(state, :output) do
 
-  end
-  def step(state, {:address, ident}) do
+  # end
+  # def step(state, {:address, ident}) do
 
-  end
-  def step(state, :end) do
+  # end
+  # def step(state, :end) do
 
-  end
+  # end
   def step(state, op) do
     {:error, "Unrecognized op #{inspect op}"}
   end
